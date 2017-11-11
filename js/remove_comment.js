19ae1c3e252067057ab764e2cab030ef196eb484
Anondo <aanondos@gmail.com>
2017-11-11 17:34:32 +0600

    aiub_project: added ajax call for commment deletion

---
+++ js/remove_comment.js
@@ -0,0 +1,23 @@
+function rmvComment(id)
+{
+    var flag = confirm("Are You Sure You Want To Remove This Comment?");
+    if(flag)
+    {
+        var comment = document.getElementById(id);
+        var ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function()
+        {
+            if(this.readyState == 4 && this.status == 200)
+            {
+                var ok = this.responseText;
+                if(ok == "ok")
+                    comment.parentNode.removeChild(comment);
+                else
+                    alert(ok);
+            }
+        }
+        ajax.open("GET" , "http://localhost/Projects/aiub project/action/comment_delete.php/?comment_id="+id , false);
+        ajax.send();
+    }
+
+}
